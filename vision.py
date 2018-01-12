7d23ea3c12bf636a2b1a01d967d65f8768d249f7
Matt Gallant <chillinerd@gmail.com>
2018-01-12 16:14:46 -0500

    Cube Code Fixes

--- vision.py
+++ vision.py
@@ -1,5 +1,5 @@
 import cv2
-from grip_ddcard import GripPipeline
+from grip_cube import GripPipeline
 
 def extra_processing(pipeline):
     center_x_positions = []
@@ -16,21 +16,24 @@ def extra_processing(pipeline):
         heights.append(y)
     
     # Print output
-    print(center_x_positions)
-    print(center_y_positions)
-    print(widths)
-    print(heights)
+    print("center X: ", center_x_positions)
+    print("center Y: ", center_y_positions)
+    print("width: ", widths)
+    print("height: ", heights)
 
 def main():
     print('Creating Video Capture')
-    cap = cv2.imread('target.jpg')
+    cap = cv2.VideoCapture(0)
 
     print('Creating Pipeline')
     pipeline = GripPipeline()
 
     print('Running Pipeline')
-    pipeline.process(cap)
-    extra_processing(pipeline)
+    while cap.isOpened():
+        have_frame, frame = cap.read()
+        if have_frame:
+            pipeline.process(frame)
+            extra_processing(pipeline)
 
     print('Capture Closed')
 
